67cb929cd7e5c702e39b7f1e3aa7cdc2915ae522
herhe <tizips@163.com>
2023-10-19 15:27:24 +0800

    格式调整，代码优化

--- contracts/queue/queue.go
+++ contracts/queue/queue.go
@@ -1,12 +1,14 @@
 package queue
 
+import "time"
+
 type Queue interface {
 	Driver
 	Channel(channel string) (Driver, error)
 }
 
 type Driver interface {
-	Producer(body []byte, exchange string, routes []string) error
+	Producer(body []byte, exchange string, routes []string, delays ...time.Duration) error
 	Consumer(handler func(data []byte), queue string) error
 	Close() error
 }
